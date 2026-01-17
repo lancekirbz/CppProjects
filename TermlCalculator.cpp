f157f3690ee73f9ee5566b488bfc43bc80c6f00a
lancekirbz <lancekirbybbantoto@gmail.com>
2026-01-17 18:42:00 +0800

    Enhance structure and visual
    Added a header and formatted output for better user experience.

--- TermlCalculator.cpp
+++ TermlCalculator.cpp
@@ -8,42 +8,48 @@ int main(){
     double num2;
     double result;
 
-    std::cout << "Enter the operation (+, -, x, /): ";
+    std::cout << " **********CALCULATOR**********" << std::endl;
+
+
+    //inputs
+    std::cout << "  Enter the operation (+, -, x, /): ";
     std::cin >> o;
 
-    std::cout << "Enter the first number: ";
+    std::cout << "  Enter the first number: ";
     std::cin >> num1;
 
-    std::cout << "Enter the second number: ";
+    std::cout << "  Enter the second number: ";
     std::cin >> num2;
 
 
+    //calculations
     switch(o){
         case '+':
             result = num1 + num2;
-            std::cout << "result: " << result << std::endl;
+            std::cout << "  result: " << result << std::endl;
             break;
         case '-':
             result = num1 - num2;
-            std::cout << "result: " << result << std::endl;
+            std::cout << "  result: " << result << std::endl;
             break;
 
         case 'x':
             result = num1 * num2;
-            std::cout << "result: " << result << std::endl;
+            std::cout << "  result: " << result << std::endl;
             break;
 
         case '/':
             result = num1 / num2;
-            std::cout << "result: " << result << std::endl;
+            std::cout << "  result: " << result << std::endl;
             break;
         default:
-            std::cout << "Please use the right operation." << std::endl;
+            std::cout << "  Please use the right operation." << std::endl;
             break;
 
     }
-
+    std::cout << " *******************************";
 
 
     return 0;
+}   
 }   
